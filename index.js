b54e8185ed7c34e2cf377660bf5119b7a072babb
echolevel <echolevel@gmail.com>
2017-01-23 20:57:09 +0000

    test2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var request = require('request');
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
@@ -248,6 +248,6 @@ app.get('/', function(req, res) {
 })
 
 
-app.listen(8086, function() {
-  console.log('Listening on 8086');
+app.listen(80, function() {
+  console.log('Listening on 80');
 })
